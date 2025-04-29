01c1a3fe0f80d708c9118bce2aeed4bf575654a8
Lyra <nmlyra13@gmail.com>
2025-04-29 20:32:21 -0300

    using array - bubble sort

--- Bubble.java
+++ Bubble.java
@@ -4,17 +4,38 @@ public class Bubble {
 
 	public static void main(String[] args) {
 
-		int nums[]= {99, -10, 100123, 18, -978, 5623, 463, -9, 287, 49};
-		int a,b,c;
+		int nums[] = { 99, -10, 100123, 18, -978, 5623, 463, -9, 287, 49 };
+		int a, b, t;
 		int size;
-		
+
 		size = 10; // número de elementos a serem classificados;
-		
-		// exibe o array original
+
+		// exibe o array original.
 		System.out.println("Original array is:");
-		for (int i=0; i< size; i++) {
-			
+		for (int i = 0; i < size; i++) {
+			System.out.println("nums[" + i + "]: " + nums[i]);
 		}
+
+		// esta é a classificação por bolha.
+		for (a = 1; a < size; a++) {
+			for (b = size - 1; b >= a; b--) {
+				if (nums[b - 1] > nums[b]) {
+					// troca elemento
+					t = nums[b - 1];
+					nums[b - 1] = nums[b];
+					nums[b] = t;
+				}
+
+			}
+		}
+
+		// exibe o array classificado
+		System.out.println("Sorted array is:");
+		for (int i = 0; i < size; i++) {
+			System.out.println(" " + nums[i]);
+			System.out.println();
+		}
+
 	}
 
 }
