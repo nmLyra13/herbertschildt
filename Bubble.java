67c7940c129815a61a1c1fb64e97e8eab7556600
Lyra <nmlyra13@gmail.com>
2025-04-29 20:05:02 -0300

    using array - bubble sort

---
+++ Bubble.java
@@ -0,0 +1,20 @@
+package herbertschildt;
+
+public class Bubble {
+
+	public static void main(String[] args) {
+
+		int nums[]= {99, -10, 100123, 18, -978, 5623, 463, -9, 287, 49};
+		int a,b,c;
+		int size;
+		
+		size = 10; // número de elementos a serem classificados;
+		
+		// exibe o array original
+		System.out.println("Original array is:");
+		for (int i=0; i< size; i++) {
+			
+		}
+	}
+
+}
