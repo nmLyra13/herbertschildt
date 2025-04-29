a3676d661a06d5690d3248e6c824744180cba032
Lyra <nmlyra13@gmail.com>
2025-04-29 19:56:18 -0300

    Exemplo ArrayDemo: percorre vetor e imprime elementos

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -4,6 +4,16 @@ public class ArrayDemo {
 
 	public static void main(String[] args) {
 
+		int sample[] = new int[10];
+		int i;
+
+		for (i = 0; i < 10; i++) {
+			sample[i] = i;
+		}
+
+		for (i = 0; i < 10; i++) {
+			System.out.println("This is sample [" + i + "]: " + sample[i]);
+		}
 	}
 
 }
