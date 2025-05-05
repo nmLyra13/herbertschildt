86eda5ef523322ba5daac563db7ea455b8820213
Lyra <nmlyra13@gmail.com>
2025-05-04 21:00:11 -0300

    Arrays Irregulares

--- Ragged.java
+++ Ragged.java
@@ -32,6 +32,26 @@ public class Ragged {
 			for (j = 0; j < 10; j++) {
 				riders[i][j] = i + j + 10;
 			}
+			for (i = 5; i < 7; i++) {
+				for (j = 0; j < 2; j++) {
+					riders[i][j] = i + j + 10;
+				}
+			}
+			System.out.println("Riders per trip during the  week: ");
+			for (i = 0; i < 5; i++) {
+				for (j = 0; j < 10; j++) {
+					System.out.println(riders[i][j] + " ");
+					System.out.println();
+				}
+				System.out.println();
+				System.out.println("Riders per trip on the weekend:");
+				for (i = 5; i < 7; i++) {
+					for (j = 0; j < 2; j++) {
+						System.out.println(riders[i][j] + " ");
+						System.out.println();
+					}
+				}
+			}
 
 		}
 	}
